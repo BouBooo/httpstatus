d4b866e00fd80833595c607bb6d0fa7cd03d7c3e
BouBooo <flonicolas.pro@outlook.fr>
2019-02-21 14:20:55 +0100

    create_database (4sites + 1admin)

---
+++ create_database.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS nicolas_lecossec;
+use nicolas_lecossec;
+
+
+CREATE TABLE IF NOT EXISTS admins
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    api_key VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS sites
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    url VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS status
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    site_id INT NOT NULL,
+    code INT NOT NULL,
+    date_report DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+INSERT INTO sites (name, url) VALUES ('Facebook', 'https://facebook.com');
+INSERT INTO sites (name, url) VALUES ('Youtube', 'https://youtube.com');
+INSERT INTO sites (name, url) VALUES ('Florent', 'http://florentnicolas.com');
+INSERT INTO sites (name, url) VALUES ('Github', 'https://github.com');
+
+INSERT INTO admins (email, password, api_key) VALUES ('deschaussettes@yopmail.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'abcdefghjaimelesapis');
